ff897583f223bb7d9ed47c02315e54b29a8833a7
“tiglot1” <tiglot@gmail.com>
2024-07-10 23:14:37 -0300

    added control case for future exercises

--- arr_review.c
+++ arr_review.c
@@ -3,36 +3,53 @@
 
 #define N 10
 
-void reverse_array(int arr[N]);
-void seen_digit(long n);
+void reverse_array();
+void seen_digit();
 
 int main(int argc, char *argv[])
 {
-//    int arr[N];
-//   printf("Insert numbers into the array:\n");
-    //   for (int j = 0; j < N; j++
-    //{
-      //  scanf("%d", &arr[j]);
-    //}
-    //reverse_array(arr);
-    
-    long n;
-    printf("Now choose a number of your liking:");
-    scanf("%ld", &n);
-    seen_digit(n);
+    int choice;
+    printf("what program would you like to run?:\n");
+    printf("1 - reverse_array\n");
+    printf("2 - seen_digit\n");
+    scanf("%d", &choice);
+    switch (choice) 
+    {
+        case 1:
+            reverse_array();
+            break;
+        case 2:
+            seen_digit();
+            break;
+        default:
+            printf("invalid choice\n");
+            break;
+    }
     return 0;
 }
 
-void reverse_array(int arr[N])
+void reverse_array()
 {
-     for (int i = N - 1; i >= 0; i--) 
+    int arr[N];
+    printf("Insert numbers into the array:\n");
+    for (int j = 0; j < N; j++)
+    {
+        scanf("%d", &arr[j]);
+    }
+
+    for (int i = N - 1; i >= 0; i--) 
     {
         printf("%d, ", arr[i]);
     }
+    printf("\n");
 }
 
-void seen_digit(long n)
+void seen_digit()
 {
+    long n;
+    printf("Now choose a number of your liking:");
+    scanf("%ld", &n);
+
     bool digit_repeat[10] = {false};
     int digit;
     int which;
