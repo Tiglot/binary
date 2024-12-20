8777a2934987ee4509e33f2975ef0362621122da
“tiglot1” <moraispedro758@gmail.com>
2024-12-20 10:02:29 -0300

    clip issues

--- enum_review.c
+++ enum_review.c
@@ -2,9 +2,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void country_codes (int code);
+
 int main(int argc, char *argv[])
 {
-    struct { int x, y;} x; 
-    struct { int x, y;} y; 
+ 
     return 0;
 }
+
+void country_codes (int code)
+{
+    typedef enum countries 
+    {
+        usa = 1,
+        russia = 7,
+        france = 33,
+        brazil = 55, 
+        japan = 81,
+    }
+    switch (code) 
+    {
+        case usa:
+           printf("usa\n");
+           break;
+        case russia:
+           printf("russia\n");
+           break;
+        case france:
+           printf("france\n");
+           break;
+        case brazil:
+           printf("brazil\n");
+           break;
+        case japan:
+           printf("japan\n");
+           break;
+        default:
+           printf("INVALID\n");
+           break;
+    }
+}       
